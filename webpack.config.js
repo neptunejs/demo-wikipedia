e60f68a8461fffbedfe58e8417de2485b566661b
Michaël Zasso <targos@protonmail.com>
2017-02-20 16:19:34 +0100

    update publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -26,8 +26,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[chunkhash].js',
-        publicPath: '/'
+        filename: '[name].[chunkhash].js'
     },
     module: {
         rules: [
