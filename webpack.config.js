4f09e6e503fedb511e3a3b04bd67ba7c5b59d3e9
Michaël Zasso <targos@protonmail.com>
2017-02-20 16:08:09 +0100

    deploy script

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,53 @@
 'use strict';
 
+const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const VENDOR_LIBS = [
+    'openchemlib/full',
+    'openchemlib-extended/lib/extend',
+    'react',
+    'react-dom',
+    'react-list',
+    'react-modal',
+    'react-ocl',
+    'react-redux',
+    'redux',
+    'redux-promise-middleware',
+    'redux-thunk',
+    'reselect'
+];
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        bundle: './src/index.js',
+        vendor: VENDOR_LIBS
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+        filename: '[name].[chunkhash].js',
         publicPath: '/'
     },
     module: {
         rules: [
             {
+                loader: 'babel-loader',
                 test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
+                exclude: /node_modules/
             }
         ]
     },
-    devtool: 'source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true
-    }
+    // devtool: 'source-map',
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            names: ['vendor', 'manifest']
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        })
+    ]
 };
